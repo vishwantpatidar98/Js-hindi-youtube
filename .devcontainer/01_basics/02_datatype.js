9adbcd30281a9418a91c1ff10ccb33f2dde8304b
vishwantpatidar98 <vishwant.patidar98@gmail.com>
2025-09-07 16:00:29 +0530

    datatype

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat all JS code as newer version
+
+//alert(3 + 3) we are using nodejs not browser
+
+console.log(3 
+    +3); //code readability is high
+console.log("my name is vishnu");
+
+let name = "vishnu"
+let age = 18
+let isLoggedIn = false
+let state; // undefined
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
