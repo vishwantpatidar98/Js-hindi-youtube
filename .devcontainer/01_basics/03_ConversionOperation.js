ee343e5bd13fc804a6bb8ce5a792752df0fc2a2f
vishwantpatidar98 <vishwant.patidar98@gmail.com>
2025-09-08 02:05:54 +0530

    03_ConversionOperation.js

--- .devcontainer/01_basics/03_ConversionOperation.js
+++ .devcontainer/01_basics/03_ConversionOperation.js
@@ -6,9 +6,9 @@ console.log(typeof (score));
 let valueInNumber = Number(score)
 console.log(valueInNumber);
 
-//"33" => 33
-//"33abc" => NaN
-//true => 1; false => 0
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
 
 
 let isLoggedIn = "vishnu"
@@ -20,8 +20,50 @@ console.log(booleanIsLoggedIn);
 //"" => false
 // "vishnu" => true
 
+
 let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log( typeof stringNumber);
+
+
+//*******************Operations*******************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " vishnu"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + "2");
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4 )* 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
