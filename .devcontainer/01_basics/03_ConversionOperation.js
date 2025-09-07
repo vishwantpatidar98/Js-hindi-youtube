75f987ead0bd15240c97955c1e6b283d70b1069f
vishwantpatidar98 <vishwant.patidar98@gmail.com>
2025-09-07 19:18:50 +0530

    Conversion of datatype

---
+++ .devcontainer/01_basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+
+let isLoggedIn = "vishnu"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "vishnu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
