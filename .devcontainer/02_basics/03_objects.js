befcce780fd17da719d06f1e2e07db82010dc9e0
vishwantpatidar98 <vishwant.patidar98@gmail.com>
2025-09-21 13:42:17 +0530

    objects

---
+++ .devcontainer/02_basics/03_objects.js
@@ -0,0 +1,38 @@
+// singleton
+
+// object literals
+
+const mySym = Symbol("Key1")
+
+const JsUser = {
+    name: "vishnu",
+    "full nmae": "Vishnu Patidar",
+    [mySym]: "myKey1",
+    age: 20,
+    location: "Balipur",
+    email: "vishnu@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full nmae"]);
+console.log( JsUser [mySym]);
+
+
+JsUser.email = "vishnu@google.com"
+// Object.freeze(JsUser)
+JsUser.email = "vishnu@microsoft.com"
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}   
+JsUser.greetingTwo = function(){
+    console.log('Hello JS user, {this.name}');
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
